c686587aa4a332c84756b153b0b72b596a3ee3db
Kristian Rasmussen <me@krx.io>
2020-02-19 10:19:41 +0100

    adding setup file

---
+++ setmeup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+shopt -s extglob nullglob 
+for f in .*; do
+if [ $f = "." ]; then continue; fi
+if [ $f = ".." ]; then continue; fi
+if [ -f "~/$f" ]; then
+    echo "$f exist";
+else 
+  echo "$f does not exist - linking"
+  file="`pwd`/$f"
+  echo "$file"
+  ln -s $file ~/$f
+fi
+done
+BIN=bin
+if [ -f "~/$BIN" ]; then
+    echo "$BIN exist"
+else 
+  echo "$BIN does not exist - linking"
+  file="`pwd`/$BIN"
+  echo "$file"
+  ln -s $file ~/$BIN
+fi
