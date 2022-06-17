20ea03418e5febcd68d5079562484e7e08a4c4c7
Kristian Rasmussen <me@krx.io>
2022-06-17 10:17:57 +0200

    more vim setup

--- setmeup.sh
+++ setmeup.sh
@@ -23,3 +23,18 @@ else
   echo "$file"
   ln -s $file ~/$BIN
 fi
+
+VIM=.vim/pack/plugins/start
+if [ -f "~/$VIM" ]; then
+    echo "$VIM exist"
+else
+  echo "$VIM does not exist - creating"
+  mkdir -p ~/$VIM
+  git clone --depth=1 https://github.com/ervandew/supertab.git ~/.vim/pack/plugins/start/supertab
+  git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp
+  git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+  git clone --depth=1 https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
+  git clone --depth=1 https://github.com/tpope/vim-endwise.git ~/.vim/pack/plugins/start/endwise
+  git clone --depth=1 https://github.com/tpope/vim-fugitive.git ~/.vim/pack/plugins/start/fugitive 
+  git clone --depth=1 https://github.com/tpope/vim-surround.git ~/.vim/pack/plugins/start/surround
+fi
